6913620d61230490391f45756e05cc6298fe9a76
Ingo Haschler <ih@ingohaschler.de>
2021-02-18 19:26:32 +0100

    First working "parallel" version :-)

--- blast/02-blast-chest.py
+++ blast/02-blast-chest.py
@@ -1,5 +1,6 @@
 from mindstorms import MSHub, Motor
 import hub
+import time
 from time import sleep
 
 # Create your objects here.
@@ -14,18 +15,47 @@ animation_scanning = ['00000:00000:56789:00000:00000',
                         '00000:00000:78932:00000:00000',
                         '00000:00000:67891:00000:00000']
 
-chest_motor = Motor('D')
+# cf. https://github.com/maarten-pennings/Lego-Mindstorms/blob/main/ms4/faq.md#what-is-motorget
+class MotorAsync:
+    def __init__(self, port):
+        self.motor = Motor(port)
+        
+    def run_for_degrees(self, degrees):
+        direction = 1
+        if(degrees < 0):
+            direction = -1
+        
+        # default speed is 75...
+        speed = direction * 20
 
-async def run_animation(frames, clear=False, delay=500, loop=True, fade=4):
+        self.motor.set_degrees_counted(0)
+
+        # start motor at speed +/-20
+        self.motor.set_default_speed(speed)
+        self.motor.start()
+        while True:
+            if(direction * ((self.motor.get_degrees_counted()) - degrees) < 0):
+                yield self.motor.get_degrees_counted()
+            else:
+                break
+        # stop motor
+        self.motor.stop()
+        return
+
+chest_motor = MotorAsync('D')
+
+def run_animation(frames, clear=False, delay_ms=500, loop=True, fade=4):
     while True:
         for f in frames:
+            start_time_ms = time.ticks_us()
             img = hub.Image(f)
             hub.display.show(img)
-            sleep(delay/1000)
-            yield
+            while((time.ticks_us() - start_time_ms) / 1000 < delay_ms):
+                #print((time.ticks_us() - start_time_us) * 1000)
+                yield
         if not loop:
             return
-        
+      
 async def run_move():
     # FIXME make motor movement async...
     chest_motor.run_for_degrees(500)
@@ -40,7 +70,8 @@ mshub.status_light.on('red')
 hub.display.clear()
 hub.display.rotation(90)
 anim=run_animation(animation_scanning)
-move=run_move()
+#move=run_move()
+move=chest_motor.run_for_degrees(500)
 while True:
     try:
         anim.send(None)
@@ -52,5 +83,6 @@ while True:
         move.send(None)
     except StopIteration as e:
         print('move finished')
+        # FIXME stops animation as well...
         break
     
